9d002427c824ca9e0c071c5a97a60c4b356eafd5
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-11 20:32:21 +0100

    Change background sh

--- InfluxDB_Zeitreihen/background.sh
+++ InfluxDB_Zeitreihen/background.sh
@@ -1,4 +1,12 @@
 #/bin/bash
 
-docker --version
-echo "done"
+docker run -d --name influxdb -p 8086:8086 influxdb:2.1.1
+docker exec -it influxdb /bin/bash 
+influx setup \
+  --org dwh-org \
+  --bucket dwh-bucket \
+  --username dwh-user \
+  --password dwhPassword123 \
+  --force  
+
+echo $?
