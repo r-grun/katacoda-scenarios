799109a9dd8e59d5fe22ea7f77aa2445d2b4002d
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-11 20:21:23 +0100

    Add test to foreground script

--- InfluxDB_Zeitreihen/foreground.sh
+++ InfluxDB_Zeitreihen/foreground.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+docker --version
 printf 'This is a bash shell script.\n\n'
 printf 'Any commands you put in here will be executed at runtime.\n\n'
 printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
