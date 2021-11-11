056b87d5869d333ee4b2aa49c1693d50bed1fb49
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-11 20:42:57 +0100

    fix setup

--- InfluxDB_Zeitreihen/background.sh
+++ InfluxDB_Zeitreihen/background.sh
@@ -2,11 +2,4 @@
 
 docker run -d --name influxdb -p 8086:8086 influxdb:2.1.1
 docker exec -it influxdb /bin/bash 
-influx setup \
-  --org dwh-org \
-  --bucket dwh-bucket \
-  --username dwh-user \
-  --password dwhPassword123 \
-  --force  
-
-echo $?
+influx setup --org dwh-org --bucket dwh-bucket --username dwh-user --password dwhPassword123 --force  
