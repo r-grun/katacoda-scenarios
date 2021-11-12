8a4c0dbbe6b457cc33c8f38d121437cbd70f792b
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-12 13:57:55 +0100

    add textfile

--- InfluxDB_Zeitreihen/foreground.sh
+++ InfluxDB_Zeitreihen/foreground.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-unzip dataset_lineProtocol.zip
 docker run -d --name influxdb -p 8086:8086 influxdb:2.1.1
+docker cp "dataset_lineProtocol.txt" influxdb:"./"
 docker exec -it influxdb /bin/bash 
