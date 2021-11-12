47a674d5e920d17971e2dde203f7cdd32d506413
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-12 18:32:39 +0100

    Remove influx setup from script

--- InfluxDB_Zeitreihen/foreground.sh
+++ InfluxDB_Zeitreihen/foreground.sh
@@ -3,5 +3,3 @@
 docker run -d --name influxdb -p 8086:8086 influxdb:2.1.1
 docker cp "dataset_lineProtocol.txt" influxdb:"./"
 docker exec -it influxdb /bin/bash 
-wait
-influx setup --org dwh-org --bucket dwh-bucket --username dwh-user --password dwhPassword123 --force && export INFLUX_ORG=dwh-org && export INFLUX_BUCKET_NAME=dwh-bucket
