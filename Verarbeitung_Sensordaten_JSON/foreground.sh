23a8aebd8445549311e640146fab092d25acaddf
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-14 17:03:01 +0100

    Add script

--- Verarbeitung_Sensordaten_JSON/foreground.sh
+++ Verarbeitung_Sensordaten_JSON/foreground.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
-printf 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
+docker run -d --name postgres -p5432:5432 -e POSTGRES_PASSWORD=mysecretpassword postgres
+docker cp "pollution_data.json" postgres:"./"
+docker exec -it postgres /bin/bash
