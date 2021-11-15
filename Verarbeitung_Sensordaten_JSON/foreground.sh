3c1d21b0af7d255f545376eccd58dd7aa36be6ce
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-11-15 16:42:18 +0100

    add wait

--- Verarbeitung_Sensordaten_JSON/foreground.sh
+++ Verarbeitung_Sensordaten_JSON/foreground.sh
@@ -3,4 +3,5 @@
 docker run -d --name postgres -p5432:5432 -e POSTGRES_PASSWORD=mysecretpassword postgres
 docker cp "pollution_data.json" postgres:"./"
 docker exec -it postgres /bin/bash
+wait
 psql -U postgres
